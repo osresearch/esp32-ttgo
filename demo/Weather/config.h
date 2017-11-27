3390a054853cc1b037111bc6ca98e1bad3ba4dbc
Trammell hudson <hudson@trmm.net>
2017-11-27 08:14:42 -0500

    use official wunderground icons (50x50) and split config into config.h

---
+++ demo/Weather/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/*
+ * Put your configuration here.
+ */
+String apikey   = "";
+String country  = "US"; //e.g. "UK";
+String city     = "NYC";    //e.g. "LONDON";
+
+const char* host     = "api.wunderground.com";
+const char* ssid     = "";
+const char* password = "";
+
